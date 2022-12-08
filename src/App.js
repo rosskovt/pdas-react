c7449c54cfa338ad4b1def6ff6f28a30ecf8b07e
rosskovt <rostikk@gmail.com>
2022-12-08 18:12:09 +0200

    completed version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png'
+import CortanaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
 
 function App() {
     return (
         <div>
-            <div>Personal Digital Assistants</div>
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
+            <div>
+                <section className='hero is-primary'>
+                    <div className='hero-body'>
+                        <p className='title'>
+                            Personal Digital Assistants
+                        </p>
+                    </div>
+                </section>
+            </div>
+            <div className='container'>
+                <section className='section'>
+                    <div className='columns'>
+                        <div className='column is-4'>
+                            <ProfileCard
+                                title="Alexa"
+                                handle="@alexa99"
+                                image={AlexaImage}
+                                description="Alexa was created by Amazon and helps you to buy things"
+                            />
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard
+                                title="Cortana"
+                                handle="@cortana32"
+                                image={CortanaImage}
+                                description="Cortana was created by Microsoft and helps you to buy things"
+                            />
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard
+                                title="Siri"
+                                handle="@siri01"
+                                image={SiriImage}
+                                description="Siri was created by Apple and helps you to buy things"
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
